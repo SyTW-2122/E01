14b98d80693484d9d112d2e975b54d66364c8c87
Yago Pérez Molanes <72870696+alu0101254678@users.noreply.github.com>
2022-02-08 17:32:45 +0000

    Añadidos ficheros raíz y carpeta src

---
+++ src/app/services/token-interceptor.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TokenInterceptorService } from './token-interceptor.service';
+
+describe('TokenInterceptorService', () => {
+  let service: TokenInterceptorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TokenInterceptorService);
+  });
+});
