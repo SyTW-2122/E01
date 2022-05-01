226d49ac5dc6be9ff39aedc37afaff5f8c4eba5c
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2022-05-01 18:09:06 +0000

    Realizado el formulario

--- frontend/src/app/components/anadir-productos/anadir-productos.component.ts
+++ frontend/src/app/components/anadir-productos/anadir-productos.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 import { ProductosService } from '../../services/productos.service';
+
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
 
 @Component({
   selector: 'app-anadir-productos',
@@ -8,7 +13,10 @@ import { ProductosService } from '../../services/productos.service';
 })
 export class AnadirProductosComponent implements OnInit {
 
-  constructor(private misprods: ProductosService) { }
+  productoSelected: string | ArrayBuffer;
+  file: File
+
+  constructor(private misprods: ProductosService, private router: Router) { }
 
   ngOnInit(): void {
     this.getproductos();
@@ -23,6 +31,21 @@ export class AnadirProductosComponent implements OnInit {
         console.log("Error en mis productos");
       }
     )
+  }
+
+  onProductoSelected(event: HtmlInputEvent): void {
+    if (event.target.files && event.target.files[0]) {
+      this.file = <File>event.target.files[0];
+      // image preview
+      const reader = new FileReader();
+      reader.onload = e => this.productoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+  uploadProducto(title: HTMLInputElement, price: HTMLInputElement, description: HTMLTextAreaElement): boolean {
+    console.log(title.value, price.value, description.value);
+    return false;
   }
 
 }
